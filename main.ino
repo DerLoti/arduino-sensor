460e254db1644706f73a93804da8706085b029a0
AronikTV / Lennox <68695323+AronikTV@users.noreply.github.com>
2023-03-20 08:53:46 +0100

    Update and rename Display.ino to main.ino

--- main.ino
+++ main.ino
@@ -11,10 +11,6 @@ long duration, cm, inches;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// defines pins numbers
-
-
-
 void setup() {
   lcd.begin(16, 2);
   Serial.begin(9600);
