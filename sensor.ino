8f23ffa41c6525df749042238713d1c1a9302b9f
AronikTV / Lennox <68695323+AronikTV@users.noreply.github.com>
2023-03-20 09:01:12 +0100

    Cleaned Up Code

--- sensor.ino
+++ sensor.ino
@@ -1,34 +1,31 @@
-void sounds () {
+void sounds() {
   if (cm <= 5) {
-    for (int i=0; i <= 5; i++){
+    for (int i = 0; i <= 5; i++) {
       tone(8, 2000);
       digitalWrite(redLed, HIGH);
       delay(100);
       noTone(8);
       digitalWrite(redLed, LOW);
-            delay(100);
-   } 
-  }
-  else if(cm <= 15) {
-    for (int i=0; i <= 5; i++){
+      delay(100);
+    }
+  } else if (cm <= 15) {
+    for (int i = 0; i <= 5; i++) {
       tone(8, 1500);
       digitalWrite(yellowLed, HIGH);
       delay(100);
       noTone(8);
       digitalWrite(yellowLed, LOW);
-                              delay(100);
-   }  
-                               
-  }
-  else if(cm <= 30) {
-      for (int i=0; i <= 5; i++){
+      delay(100);
+    }
+
+  } else if (cm <= 30) {
+    for (int i = 0; i <= 5; i++) {
       tone(8, 1200);
       digitalWrite(greenLed, HIGH);
       delay(100);
       noTone(8);
       digitalWrite(greenLed, LOW);
-            delay(100);
-     
-    }  
+      delay(100);
+    }
   }
 }
