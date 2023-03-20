e5c3f0128952af0af877a85af70983a372c075f8
AronikTV / Lennox <68695323+AronikTV@users.noreply.github.com>
2023-03-20 08:48:04 +0100

    Added for Loops
    Added for Loops to make code more efficient.

--- sounds.ino
+++ sounds.ino
@@ -1,28 +1,27 @@
 void sounds () {
   if (cm <= 5) {
-    
+    for (int i=0; i <= 5; i++){
       tone(8, 2000);
       digitalWrite(redLed, HIGH);
       delay(100);
       noTone(8);
       digitalWrite(redLed, LOW);
             delay(100);
- 
-    
+   } 
   }
   else if(cm <= 15) {
-    
+    for (int i=0; i <= 5; i++){
       tone(8, 1500);
       digitalWrite(yellowLed, HIGH);
       delay(100);
       noTone(8);
       digitalWrite(yellowLed, LOW);
                               delay(100);
-     
+   }  
                                
   }
   else if(cm <= 30) {
-      
+      for (int i=0; i <= 5; i++){
       tone(8, 1200);
       digitalWrite(greenLed, HIGH);
       delay(100);
@@ -30,84 +29,6 @@ void sounds () {
       digitalWrite(greenLed, LOW);
             delay(100);
      
-      
-  }
-  else if(cm <= 60) {
-      tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);      tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      delay(100);
-            tone(8, 600);
-      delay(100);
-      noTone(8);
-      
-      
+    }  
   }
 }
